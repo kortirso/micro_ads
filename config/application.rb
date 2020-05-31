c1391b5fd3339e0235c0eb345f9ea40d7821ea68
kortirso <kortirso@gmail.com>
2020-05-31 18:16:55 +0300

    fix

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,18 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'api'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'app'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'boot'
 
 Bundler.require :default, ENV['RACK_ENV']
 
-Dir[File.expand_path('../../api/*.rb', __FILE__)].each do |f|
-  require f
-end
-
 # database config
-require_relative '../app/setup'
+require 'setup'
 # database models
-require_relative '../app/models'
+require 'models'
 # serializers
-require_relative '../app/serializers'
+require 'serializers'
 
 # api endpoints
-require_relative '../app/app'
+require 'app'
