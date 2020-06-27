a7f7627353967554da459262b824f76cb2a3ac65
kortirso <kortirso@gmail.com>
2020-06-27 14:49:30 +0300

    remove cgi

--- app/lib/geocoder_service/geocode.rb
+++ app/lib/geocoder_service/geocode.rb
@@ -3,7 +3,7 @@
 module GeocoderService
   module Geocode
     def geocode_later(ad)
-      payload = { id: ad.id, city: CGI.escape(ad.city) }.to_json
+      payload = { id: ad.id, city: ad.city }.to_json
       publish(payload, type: 'geocode')
     end
   end
