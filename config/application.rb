82430e34137b8cb265f21947eaa756cbabbde582
kortirso <kortirso@gmail.com>
2020-07-12 07:25:47 +0300

    fi

--- config/application.rb
+++ config/application.rb
@@ -3,19 +3,21 @@
 class Application < Grape::API
   mount ::Api::V1
 
-  helpers do
-    def logger
-      API.logger
-    end
-  end
-
   class << self
+    attr_accessor :logger
+
     def root
       File.expand_path('..', __dir__)
     end
 
     def environment
       ENV['RACK_ENV'].to_sym
+    end
+  end
+
+  helpers do
+    def logger
+      API.logger
     end
   end
 end
