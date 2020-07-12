affa2694453f009e60d150cadac2ce78718c29b5
kortirso <kortirso@gmail.com>
2020-07-12 07:22:05 +0300

    modify logger initializer

--- config/application.rb
+++ config/application.rb
@@ -3,27 +3,19 @@
 class Application < Grape::API
   mount ::Api::V1
 
-  def self.root
-    File.expand_path('..', __dir__)
-  end
-
-  def self.environment
-    ENV['RACK_ENV'].to_sym
-  end
-
-  logger Ougai::Logger.new(
-    root.concat('/', Settings.logger.path),
-    level: Settings.logger.level
-  )
-
-  logger.before_log = lambda do |data|
-    data[:service] = { name: Settings.app.name }
-    data[:request_id] ||= Thread.current[:request_id]
-  end
-
   helpers do
     def logger
       API.logger
+    end
+  end
+
+  class << self
+    def root
+      File.expand_path('..', __dir__)
+    end
+
+    def environment
+      ENV['RACK_ENV'].to_sym
     end
   end
 end
--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 
+logger = Ougai::Logger.new(
+  Application.root.concat('/', Settings.logger.path),
+  level: Settings.logger.level
+)
+
+logger.before_log = lambda do |data|
+  data[:service] = { name: Settings.app.name }
+  data[:request_id] ||= Thread.current[:request_id]
+end
+
+Application.logger = logger
+
 Sequel::Model.db.loggers.push(Application.logger)
